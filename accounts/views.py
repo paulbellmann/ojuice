4abff58898e65a191637255bd4a31d55700647f2
paulbellmann <paul.bellmann@yahoo.de>
2017-07-05 20:25:11 +0200

    + make messages better

--- accounts/views.py
+++ accounts/views.py
@@ -9,8 +9,11 @@ from django.contrib.auth.decorators import login_required
 def log_in(request):
     context = {
         'title': 'Log In'
-    } 
-    return render(request, 'login.html', context)
+    }
+    if not request.user.is_authenticated:
+        return render(request, 'login.html', context)
+    else:
+        return redirect('index')
 
 def log_on(request):
     username = request.POST['username']
@@ -18,13 +21,14 @@ def log_on(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        messages.add_message(request, messages.SUCCESS, "Successfully logged in.")
+        messages.add_message(request, messages.SUCCESS, "You are now logged in.")
         return redirect('index')
     else:
-        messages.add_message(request, messages.WARNING, "Wrong Password or Username!")
+        messages.add_message(request, messages.WARNING, "Password or Username is wrong.")
         return redirect('login')
 
 @login_required
 def log_out(request):
     logout(request)
+    messages.add_message(request, messages.INFO, "Succesfully logged out.")
     return redirect('index')
