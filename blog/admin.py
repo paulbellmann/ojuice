1542704cb4a134c8866f97dd43c6bd8e2e83b931
paulbellmann <paul.bellmann@yahoo.de>
2017-07-06 21:36:39 +0200

    + show fields in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -6,4 +6,7 @@ from django.contrib import admin
 from .models import Todo
 
 # Register your models here.
-admin.site.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'body', 'owner', 'created_at')
+
+admin.site.register(Todo, TodoAdmin)
