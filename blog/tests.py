0c05321228ef4237c275508e582efc2650239429
paulbellmann <paul.bellmann@yahoo.de>
2017-07-07 20:58:12 +0200

    + try to write a test

--- blog/tests.py
+++ blog/tests.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+class BlogViewsTestCase(TestCase):
+    """tests blog app"""
+    def test_index(self):
+        resp = self.client.get('/blog/')
+        self.assertEqual(resp.status_code, 302)
+
+    def test_create_todo(self):
+        todo = Todo.objects.create(
+            title='Do sports',
+            body='Tests this Todo-Body',
+            owner=1
+        )
+        self.assertEqual(todo, Todo.objects.get(title='Do sports'))
