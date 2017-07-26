1a2a08a08fc6d83409be03b0bbb291d41bb11343
paulbellmann <paul.bellmann@yahoo.de>
2017-07-26 17:39:32 +0200

    = better auto date value without datetime module

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -10,9 +9,9 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
     title = models.CharField(max_length=30)
     body = models.CharField(max_length=255)
-    created_at = models.DateTimeField(default=datetime.now, blank=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
     checked = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return "%s: %s" % (self.title, self.body)
