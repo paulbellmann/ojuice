543556848c4b119ff030bf8d63e565c4025e54c2
paulbellmann <paul.bellmann@yahoo.de>
2018-03-13 19:48:14 +0100

    changes for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mnist.wsgi
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'd&bb247nhirbf*x9*j&e2&j0%00gv5rhw5-&5t9vxr$38*&^84'
 HASHID_FIELD_SALT = "afu%#j1j@35d&s2i(0xx5%pp)1hb##qq6j&%rd3a5&+jkt@4oo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -128,3 +129,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/todo/static'
 ]
+
+# overide database settings
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# so that we can still use sqlite locally
+try:
+    from local_settings import *
+except ImportError:
+    pass
